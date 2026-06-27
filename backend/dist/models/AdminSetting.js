c39b6bb709fd6b73434cb8112c0bb95f6d1c7a6e
Yousef Mohamed <yousefmohamed17479191@gmail.com>
2026-06-27 21:53:14 +0300

    one

--- backend/dist/models/AdminSetting.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AdminSetting = void 0;
-const mongoose_1 = require("mongoose");
-const AdminSettingSchema = new mongoose_1.Schema({
-    key: { type: String, required: true, unique: true, index: true },
-    value: { type: mongoose_1.Schema.Types.Mixed, default: {} },
-}, { timestamps: true });
-exports.AdminSetting = (0, mongoose_1.model)('AdminSetting', AdminSettingSchema);
--- backend/dist/routes/timemachine.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../middleware/auth");
-const timemachine_controller_1 = require("../controllers/timemachine.controller");
-const router = (0, express_1.Router)();
-router.get('/', auth_1.authenticate, timemachine_controller_1.getTimeMachineTimeline);
-exports.default = router;
